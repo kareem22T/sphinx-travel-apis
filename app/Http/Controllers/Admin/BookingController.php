b8763ec476fc6e5595b272f1c645a2823bb28881
unknown <kotbekareem74@gmail.com>
2024-03-29 13:12:22 +0200

    edits

--- app/Http/Controllers/Admin/BookingController.php
+++ app/Http/Controllers/Admin/BookingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Booking\Request as BookingRequest;
+use App\Models\Message;
 use Illuminate\Support\Facades\Validator;
 use App\Traits\DataFormController;
 use App\Traits\PushNotificationTrait;
