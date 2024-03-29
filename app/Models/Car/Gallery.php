95fb8c939ede8e0a79e48d443916df7a3ab0f21b
unknown <kotbekareem74@gmail.com>
2024-03-29 07:28:32 +0200

    edist

--- app/Models/Car/Gallery.php
+++ app/Models/Car/Gallery.php
@@ -5,7 +5,7 @@ namespace App\Models\Car;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Gallary extends Model
+class Gallery extends Model
 {
     use HasFactory;
     protected $fillable = [
@@ -21,5 +21,4 @@ class Gallary extends Model
     {
         return $this->belongsTo('App\Models\Car\Car', 'car_id');
     }
-
 }
