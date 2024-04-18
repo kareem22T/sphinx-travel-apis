662a92a4342168bf73b0b27de755c8ff24ee933a
kareem22T <kotbekareem74@gmail.com>
2024-04-18 04:45:02 +0200

    edits

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Setting;
 use App\Models\Tour\Tour;
+use App\Models\Hotel\Hotel;
 use App\Traits\DataFormController;
 use App\Traits\SavePhotoTrait;
 use Illuminate\Support\Facades\Validator;
