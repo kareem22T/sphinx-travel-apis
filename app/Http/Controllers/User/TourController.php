2460ffaa6c6b5e3e8d0e2b308d6ad20d6e6cadf4
kareem22T <kotbekareem74@gmail.com>
2024-04-19 05:52:29 +0200

    edits

--- app/Http/Controllers/User/TourController.php
+++ app/Http/Controllers/User/TourController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Tour\Tour;
 use App\Models\Language;
+use App\Models\Setting;
 
 class TourController extends Controller
 {
