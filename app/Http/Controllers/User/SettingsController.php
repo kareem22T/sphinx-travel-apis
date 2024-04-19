5400e3cb7ecc74a8f15558bc246c34d1d78747cc
kareem22T <kotbekareem74@gmail.com>
2024-04-19 19:30:45 +0200

    edits

--- app/Http/Controllers/User/SettingsController.php
+++ app/Http/Controllers/User/SettingsController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Settings;
+use App\Models\Setting;
 
 class SettingsController extends Controller
 {
     public function getHomeAd() {
-        $ad = Settings::where("key", "ad")->first();
+        $ad = Setting::where("key", "ad")->first();
 
         if ($ad)
             return json_encode($ad->data);
