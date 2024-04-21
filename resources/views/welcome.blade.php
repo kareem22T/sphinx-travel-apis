99cb988a4b8cd150864583f58809978b04184dea
kareem22T <kotbekareem74@gmail.com>
2024-04-22 00:42:43 +0200

    edits

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,13 +7,13 @@
     <title>Sphinx Travel Api</title>
 </head>
 <body>
-    <form action=""  id="resetPasswordForm" style="background: white;border: 0 none;border-radius: 3px;box-shadow: 0 0 15px 1px rgba(0, 0, 0, 0.4);padding: 20px 30px;box-sizing: border-box;width: 80%;margin: 0 10%;position: relative;max-width: 500px;width: 100%;margin: auto;display: flex;flex-direction: column;gap: 1rem;border-radius: 10px;margin-top: 2rem;">
+    {{-- <form action=""  id="resetPasswordForm" style="background: white;border: 0 none;border-radius: 3px;box-shadow: 0 0 15px 1px rgba(0, 0, 0, 0.4);padding: 20px 30px;box-sizing: border-box;width: 80%;margin: 0 10%;position: relative;max-width: 500px;width: 100%;margin: auto;display: flex;flex-direction: column;gap: 1rem;border-radius: 10px;margin-top: 2rem;">
         <img src="https://sphinx-travel.com/wp-content/uploads/2021/12/Sphinx-for-websit-1.png" alt="" style="margin: auto;" width="170">
         <h1 style="font-size: 23px;font-family: arial;margin: auto;color: #0e026d;">Reset Your Password</h1>
         <input id="password" name="new_password" type="password" placeholder="Old Password" style="padding: 15px;border: 1px solid #ccc;border-radius: 3px;margin-bottom: 10px;width: 100%;box-sizing: border-box;color: #2C3E50;font-size: 13px;">
         <input id="password_confirmation" name="new_password_confirmation" type="password" placeholder="New Password Confirmation" style="padding: 15px;border: 1px solid #ccc;border-radius: 3px;margin-bottom: 10px;width: 100%;box-sizing: border-box;color: #2C3E50;font-size: 13px;">
         <button style="width: 100px;background: #8dc645;font-weight: bold;color: white;border: 0 none;border-radius: 1px;cursor: pointer;padding: 10px;margin: 10px 5px;text-decoration: none;font-size: 14px;margin: auto;width: 100%;">Reset</button>
-    </form>
+    </form> --}}
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('resetPasswordForm').addEventListener('submit', function (event) {
