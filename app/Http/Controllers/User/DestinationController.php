1141a57c5f2dd8a55c8dd52d9b402ba16b9007b6
kareem22T <kotbekareem74@gmail.com>
2024-04-30 02:07:10 +0300

    edits

--- app/Http/Controllers/User/DestinationController.php
+++ app/Http/Controllers/User/DestinationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Destinations;
+use App\Models\Destination;
 
 class DestinationController extends Controller
 {
