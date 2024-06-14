b25906f3e6295778f201a9dcb438a0a01e46db95
kareem22T <kotbekareem74@gmail.com>
2024-06-14 04:05:52 +0300

    edits

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -11,7 +11,7 @@ class NotificationController extends Controller
                 // Define the constant if not already defined
             $teamId = '7A55RYWJKX'; // Replace with your Team ID
             $keyId = '82Z9QA7FVZ'; // Replace with your Key ID
-            return $privateKey = file_get_contents(storage_path('app/apns/AuthKey_82Z9QA7FVZ.p8')); // Replace with the path to your .p8 file
+            $privateKey = file_get_contents(storage_path('app/apns/AuthKey_82Z9QA7FVZ.p8')); // Replace with the path to your .p8 file
 
             // Token expiration time (typically 1 hour)
             $time = time();
@@ -25,7 +25,7 @@ class NotificationController extends Controller
             ];
 
             // Generate the JWT
-            $jwt = JWT::encode($payload, $privateKey, 'ES256', $keyId);
+            return $jwt = JWT::encode($payload, $privateKey, 'ES256', $keyId);
             // APNs URL
             $url = "https://api.push.apple.com:443/3/device/75136D69B4C0E821E3610EE64F00D2824373757F81C6B5BB7FBECE3D02CAA4E1";
 
